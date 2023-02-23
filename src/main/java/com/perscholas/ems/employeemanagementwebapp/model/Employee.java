2450b9ac05e83fd97a3c8ad6a8c2efaf1647f4b4
Samuel Pike <samuelp1dev@gmail.com>
2023-02-22 16:45:35 -0800

    first commit

---
+++ src/main/java/com/perscholas/ems/employeemanagementwebapp/model/Employee.java
@@ -0,0 +1,40 @@
+package com.perscholas.ems.employeemanagementwebapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "job_title")
+    private String jobTitle;
+
+    @Column(name = "salary")
+    private String salary;
+}
---
+++ src/test/java/com/perscholas/ems/employeemanagementwebapp/service/UserServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.perscholas.ems.employeemanagementwebapp.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+}
