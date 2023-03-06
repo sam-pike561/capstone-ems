87eca280de5f98afce0772cd64254b1b53b168ad
Samuel Pike <samuelp1dev@gmail.com>
2023-03-06 10:20:43 -0800

    custom 404 redirect

--- src/main/java/com/perscholas/ems/employeemanagementwebapp/model/Employee.java
+++ src/main/java/com/perscholas/ems/employeemanagementwebapp/model/Employee.java
@@ -3,12 +3,8 @@ package com.perscholas.ems.employeemanagementwebapp.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 
 @Entity
 @Table(name = "employees")
@@ -37,4 +33,5 @@ public class Employee {
 
     @Column(name = "salary")
     private String salary;
+
 }
